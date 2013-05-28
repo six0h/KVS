30ff8c028a25dc687fa3fd38e8f2eb00374eb1a6
Cody Halovich <chalovich@Codys-MacBook-Pro.local>
2013-05-28 13:17:11 -0700

    file validation is complete, working on blocks now

--- validator.py
+++ validator.py
@@ -18,7 +18,7 @@ class Database(object):
     def listDatabases(self):
         '''List all databases'''
         dirList = []
-        for paths,dirs,files in os.walk('/Users/chalovich/Projects/Caviar/KVS/db'):
+        for paths,dirs,files in os.walk(self.dbDir):
             for d in dirs:
                 dirList.append(d)
         return dirList
@@ -66,15 +66,16 @@ class Database(object):
         return len(self.getDbFiles()) 
 
 class DbFile(object):
+
     def __init__(self,fileName,db):
         self.file = open(fileName, 'r')
         self.FILESIZE = os.path.getsize(fileName)
         self.MAGICLEN = 8
         self.MAGICOFFSET = 0
-        self.UUIDLEN = 64 
+        self.UUIDLEN = 32
         self.UUIDOFFSET = 8
         self.INDEXLEN = 4
-        self.INDEXOFFSET = 72 
+        self.INDEXOFFSET = 40 
         self.FIELDS = ('Magic','UUID','Index')
         self.FILENAME = fileName
         self.DB = db
@@ -82,68 +83,91 @@ class DbFile(object):
         self.fileOffset = 0
         self.blocks = []
 
-        self.getBlocks()
+        # self.getBlocks()
 
     def getBlocks(self):
         i = 0
         while self.FILESIZE > self.fileOffset:
             self.blocks.append(Block(self.fileOffset,self.file))
-            blockLen = self.blocks[i]().BLOCKLENGTH
+            block = self.blocks[i]
+            blockLen = block.getTotalBlockLength()
             i = i + 1
             self.fileOffset = self.fileOffset + blockLen
 
 class Block(object):
-    def __init__(self,filePos,myFile):
-        self.FILE = myFile
+    def __init__(self,fileOffset,currentFile):
         self.VERSIONLEN = 4
         self.TIMESTAMPLEN = 8
-        self.HASHLEN = 64
+        self.HASHLEN = 32
         self.LENGTHLEN = 4
         self.LENGTHOFFSET = self.VERSIONLEN + self.TIMESTAMPLEN + self.HASHLEN
         self.BLOCKORDER = ('version','timestamp','hash','length','json')
-        self.filePos = filePos
+        self.currentFile = currentFile
+        self.fileOffset = fileOffset
         self.JSONLEN = self.getJSONLength()
         self.BLOCKLENGTH = self.getTotalBlockLength()
 
     def getJSONLength(self):
-        self.FILE.seek(self.filePos + self.LENGTHOFFSET)
-        bits = self.FILE.read(self.LENGTHLEN)
+        self.currentFile.seek(self.fileOffset + self.LENGTHOFFSET)
+        bits = self.currentFile.read(self.LENGTHLEN)
+        print bits,'file position',self.fileOffset
         return struct.unpack("<I", bits)[0]
 
     def getTotalBlockLength(self):
         return int(self.VERSIONLEN) + int(self.TIMESTAMPLEN) + int(self.HASHLEN) + int(self.LENGTHLEN) + int(self.JSONLEN)
 
-    def setFilePos(pos):
-        self.filePos = pos
+class Validator(Database):
+    def __init__(self,dir='./db/',*params):
+        if(dir != None):
+            self.dbDir = dir
+        else:
+            self.dbDir = '.'
 
-class Validator():
-    def __init__(self):
-        self.db = Database()
+        if(params != None):
+            self.useDatabase(params[0])
 
-    def validateHeader(self):
+    def validateFiles(self):
+        for currentFile in self.files:
+            self.validateFileHeader(currentFile)
+            for block in currentFile.blocks:
+                self.validateBlock(block)
+
+
+    def validateFileHeader(self,currentFile):
         '''Begin validating all Fields'''
-        while True:
-            for f in self.FIELDS:
-                if getattr(self, 'validate%s' % f)() == False:
-                    raise ValidationError("File %s does not have a valid header. The %s is broken." % (self.FILENAME, f))
-                    break
+        while currentFile.fileOffset < 44:
+            print "Current File: %s" % currentFile.FILENAME
+            for f in currentFile.FIELDS:
+                if getattr(self, 'validateFile%s' % f)(currentFile) == False:
+                    raise ValidationError("Invalid header. The %s is broken." % (f))
+                else:
+                    print "Passed: %s" % f
 
-    def validateMagic(self):
-        self.file.seek(self.MAGICOFFSET)
-        return self.file.read(self.MAGICLEN) == 'caviar01'
+    def validateFileMagic(self,currentFile):
+        '''Validate the File Magic Number'''
+        currentFile.file.seek(currentFile.MAGICOFFSET)
+        currentFile.fileOffset = currentFile.fileOffset + currentFile.MAGICLEN
+        return currentFile.file.read(currentFile.MAGICLEN) == 'caviar01'
 
-    def validateUUID(self):
-        self.file.seek(self.UUIDOFFSET)
-        UUID = self.file.read(self.UUIDLEN)
-        hexUUID = sha256(self.DB).hexdigest()
-        return UUID == hexUUID
+    def validateFileUUID(self,currentFile):
+        '''Validate the UUID'''
+        currentFile.file.seek(currentFile.UUIDOFFSET)
+        UUID = currentFile.file.read(currentFile.UUIDLEN)
+        binUUID = sha256(currentFile.DB).digest()
+        currentFile.fileOffset = currentFile.fileOffset + currentFile.UUIDLEN
+        return UUID == binUUID
 
-    def validateIndex(self):
-        self.file.seek(self.INDEXOFFSET)
-        fileName, fileIndex = os.path.splitext(self.FILENAME)
-        print "%s - %s" % (fileIndex[1:],self.file.read(self.INDEXLEN))
-        return fileIndex[1:] == self.file.read(self.INDEXLEN)
+    def validateFileIndex(self,currentFile):
+        '''Validate The File Index'''
+        currentFile.file.seek(currentFile.INDEXOFFSET)
+        fileName, fileExt = os.path.splitext(currentFile.FILENAME)
+        fileExt = int(fileExt[1:])
+        binFileIndex = struct.pack('<I', fileExt)
+        currentFile.fileOffset = currentFile.fileOffset + currentFile.INDEXLEN
+        return binFileIndex == currentFile.file.read(currentFile.INDEXLEN)
 
+    def validateBlock(self,block):
+        print "Validating:",block.VERSIONLEN
 
 class ValidationError(Exception):
     def __init__(self, value):
@@ -160,10 +184,7 @@ class DatabaseError(Exception):
         return repr(self.value)
 
 if __name__ == '__main__':
-    myParams = ('knicks')
-    db = Database('./db/',myParams)
-    dbFiles = db.getDbFiles()
-    for name in dbFiles:
-        print name
-        #currentFile = DbFile(name, db.currentDb)
-        #currentFile.validateHeader()
+    myParams = ('sample')
+    validator = Validator('./db/',myParams)
+    dbFiles = validator.getDbFiles()
+    validator.validateFiles()
